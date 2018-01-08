c9e40fa1ea24facd94fe12e13434b14f0e7fcf83
wangq <wangqian_live@163.com>
2018-01-08 18:24:08 +0800

    unit testing

---
+++ src/test/java/com/wangqian/code/util/tools/StringUtilTest.java
@@ -0,0 +1,14 @@
+package com.wangqian.code.util.tools;
+
+
+import com.wangqian.code.util.tools.util.tools.StringUtil;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StringUtilTest {
+    @Test
+    public void testHello() {
+        Assert.assertEquals(true, StringUtil.isStrEmpty(" "));
+        Assert.assertEquals(StringUtil.nvl(null), "");
+    }
+}
