8d699fabf7a342216efe5e2488c344bdca1b629a
wangq <wangqian_live@163.com>
2018-02-06 15:11:45 +0800

    add: web home html

---
+++ src/main/webapp/html/base/main/js/footer.js
@@ -0,0 +1,10 @@
+/**
+ * <p>Title: footer</p>
+ * <p>Description: footer</p>
+ * <p>Copyright: Copyright (c) 2018</p>
+ *
+ * @author wangqian
+ * @date 2018-02-06
+ * @version 1.0
+ */
+document.writeln("<footer>© Copyright 2018 Q. Walte Wang all rights reserved | <a href=\"privacy-policy.html\">Privacy Policy</a></footer>");
---
+++ src/main/webapp/html/base/main/js/header.js
@@ -0,0 +1,31 @@
+/**
+ * <p>Title: header</p>
+ * <p>Description: header</p>
+ * <p>Copyright: Copyright (c) 2018</p>
+ *
+ * @author wangqian
+ * @date 2018-02-06
+ * @version 1.0
+ */
+document.writeln("<header class=\"pure-g\" id=\"header\">");
+document.writeln("<div class=\"pure-u-1 pure-u-lg-4-24\">");
+document.writeln("<div class=\"logo\"><a href=\"/\"><img src=\"../imgs/qwaltewang-logo.svg\" class=\"pure-img\" alt=\"\" /></a></div></div>");
+document.writeln("<input type=\"checkbox\" id=\"menu-toggle-cb\">");
+document.writeln("<label id=\"menu-toggle\" for=\"menu-toggle-cb\" onclick><s class=\"bar\"></s><s class=\"bar\"></s><s class=\"bar\"></s></label>");
+document.writeln("<div class=\"pure-u-1 pure-u-lg-20-24 box-relative menu-wrapper\">");
+document.writeln("<nav class=\"pure-menu pure-menu-horizontal menu-local\">");
+document.writeln(" <ul class=\"pure-menu-list\">");
+document.writeln("   <li class=\"pure-menu-item\"><a href=\"/\" class=\"pure-menu-link\">Home</a></li>");
+document.writeln("   <li class=\"pure-menu-item\"><a href=\"/downloads\" class=\"pure-menu-link\">Downloads</a></li>");
+document.writeln("   <li class=\"pure-menu-item\"><a href=\"/about\" class=\"pure-menu-link\">About</a></li>");
+document.writeln("   <li class=\"pure-menu-item\"><a href=\"/community\" class=\"pure-menu-link\">Community</a></li>");
+document.writeln("   <li class=\"pure-menu-item\"><a href=\"/sponsors\" class=\"pure-menu-link\">Sponsors</a></li>");
+document.writeln("</ul></nav>");
+document.writeln("<nav class=\"pure-menu pure-menu-horizontal menu-external\">");
+document.writeln("<ul class=\"pure-menu-list\">");
+document.writeln("   <li class=\"pure-menu-item\"><a href=\"underConstruction.html\" class=\"pure-menu-link\">wiki</a></li>");
+document.writeln("   <li class=\"pure-menu-item\"><a href=\"https://github.com/wangqiantra\" class=\"pure-menu-link\">git</a></li>");
+document.writeln("   <li class=\"pure-menu-item\"><a href=\"underConstruction.html\" class=\"pure-menu-link\">bugs</a></li>");
+document.writeln("   <li class=\"pure-menu-item\"><a href=\"underConstruction.html\" class=\"pure-menu-link\">forums</a></li>");
+document.writeln("   <li class=\"pure-menu-item\"><a href=\"underConstruction.html\" class=\"pure-menu-link\">packages</a></li>");
+document.writeln("</ul></nav></div></header>");
